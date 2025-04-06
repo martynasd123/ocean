882da8f9ffb1e81eef004c7de68ad818b6767d37
emilijaMarija <z.mari.emilija1@gmail.com>
2025-04-06 17:17:30 +0200

    Add backgrounds

--- characters/player/states/idle_state.gd
+++ characters/player/states/idle_state.gd
@@ -8,7 +8,7 @@ func _ready():
 func _flip_direction():
 	animated_sprite.flip_h = not animated_sprite.flip_h
 
-func _process(delta):
+func _process(_delta):
 	if Input.get_axis("move_left", "move_right") != 0:
 		change_state.call("run")
 	elif (Input.is_action_pressed("jump")):
